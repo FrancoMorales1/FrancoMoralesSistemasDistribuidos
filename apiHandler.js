7e627784fde31b20b94ba90f6c79a3e76113be31
Franco Morales <francomorales0001@gmail.com>
2025-09-05 22:51:56 -0300

    First commit to branch. apiHandler done. Created files for main code and utils for errorHandler

---
+++ apiHandler.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+
+var base_url = "https://jsonplaceholder.typicode.com/";
+
+async function getUsers() {
+    let response = null;
+    try {
+        response = await axios.get(base_url+"users");
+        response = response.data;
+    } catch (err) { 
+        util.errorHandler(err);
+    }
+    return response;
+}
+
+async function getPostsWithId(user_id) {
+    let response = null;
+    try {
+        response = await axios.get(base_url+"posts?userId="+`/${user_id}`);
+        response = response.data;
+    } catch (err) { 
+        util.errorHandler(err);
+    }
+    return response;
+}
+
+module.exports = { getUsers, getPostsWithId };
